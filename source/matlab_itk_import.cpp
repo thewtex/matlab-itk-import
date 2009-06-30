029f64aeb19197427efca626e2ed60b34da72005
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-06-30 17:07:40 -0500

    add argument checking

--- source/matlab_itk_import.cpp
+++ source/matlab_itk_import.cpp
@@ -1,7 +1,13 @@
 #include "mex.h" 
-#include <iostream>
+
+
 void mexFunction(int nlhs, mxArray *plhs[],
     int nrhs, const mxArray *prhs[])
 {
-  mexPrintf("Hello Matlab world!\n"); 
+  // argument check
+  if (nrhs<1 || 
+    nrhs>2 ||
+    !mxIsChar(prhs[0]) ||
+    (nrhs == 2 && !mxIsNumeric(prhs[1])) )
+    mexErrMsgTxt("Incorrect arguments, see 'help @MATLAB_FUNCTION_NAME@'");
 } 
